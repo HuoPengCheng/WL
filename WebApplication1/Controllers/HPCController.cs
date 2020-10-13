1abeb3859626b4ffce0c31091274e3166267f479
HPC <HPC@DESKTOP-ST9CDF5>
2020-10-13 16:04:38 +0800

    12

---
+++ WebApplication1/Controllers/HPCController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Model;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class HPCController : ControllerBase
+    {
+        public WLDbcontext db;
+        public HPCController(WLDbcontext db) { this.db = db; }
+        public async Task<ActionResult<IEnumerable<Student>>> Show()
+        {
+            return await db.Student.ToListAsync();
+        }
+    }
+}
