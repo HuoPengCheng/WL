b99370f32b754283beee283389c535ea093800e9
HPC <HPC@DESKTOP-ST9CDF5>
2020-10-26 08:56:13 +0800

    1

--- WebApplication1/Model/WLDbcontext.cs
+++ WebApplication1/Model/WLDbcontext.cs
@@ -10,5 +10,7 @@ namespace WebApplication1.Model
     {
         public WLDbcontext() { }
         public WLDbcontext(DbContextOptions<WLDbcontext> options) : base(options) { }
+
+        public DbSet<Student> Student { get; set; }
     }
 }
