f3d89b49aa71a9d6688adf92d6ba3b74e79aa978
谢震 <46702380+hardshells@users.noreply.github.com>
2020-10-26 08:05:05 +0800

    10-26-xyc

---
+++ WebApplication1/Model/WorkandDepView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Model
+{
+    public class WorkandDepView
+    {
+        [Key]
+        public int WId { get; set; }
+        public int DId { get; set; }
+        public string DName { get; set; }
+        public string WName { get; set; }
+        public int WAge { get; set; }
+        public int WState { get; set; }
+        
+    }
+}
