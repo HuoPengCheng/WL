24905e9c3391ad8ffbd87db0af81ec827d5ba0b6
谢震 <46702380+hardshells@users.noreply.github.com>
2020-10-26 08:17:11 +0800

    111

--- WebApplication1/Model/WLDbcontext.cs
+++ WebApplication1/Model/WLDbcontext.cs
@@ -10,10 +10,5 @@ namespace WebApplication1.Model
     {
         public WLDbcontext() { }
         public WLDbcontext(DbContextOptions<WLDbcontext> options) : base(options) { }
-        public DbSet<Student> Student { get; set; }
-
-        public DbSet<Work> Work { get; set; }
-        public DbSet<Department> Department { get; set; }
-        public DbSet<WorkandDepView> WorkandDepView { get; set; }
     }
 }
